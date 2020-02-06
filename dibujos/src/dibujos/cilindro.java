756b7bf251d09464db45aba77262f98d270c4662
Kevin.Jimenez24 <kivito2009@hotmail.com>
2020-02-05 22:53:13 -0500

    Update cilindro.java

--- dibujos/src/dibujos/cilindro.java
+++ dibujos/src/dibujos/cilindro.java
@@ -5,34 +5,63 @@
  */
 package dibujos;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author SISTEMAS CORP
  */
-public class cilindro extends circulo{
-    private int altura;
+public class cilindro extends circulo {
 
-    public cilindro(int altura) {
-        this.altura = altura;
-    }
+    private double altura;
 
-    public cilindro(int altura, int radio, int x, int y) {
-        super(radio, x, y);
-        this.altura = altura;
-    }
-
-    public int getAltura() {
+    public double getAltura() {
         return altura;
     }
 
-    public void setAltura(int altura) {
+    public void setAltura(double altura) {
         this.altura = altura;
     }
-    
-    
-    
-    
-    
-    
-    
+
+    public cilindro(int x, int y, double radio, double altura) {
+        super(x, y, radio);
+        this.altura = altura;
+    }
+
+    public cilindro() {
+        super();
+        this.altura = Double.parseDouble(JOptionPane.showInputDialog("ingrese la altura del cilindro"));
+    }
+
+    public double getArea() {
+
+        return 2 * super.getArea() + this.getCircunferencia() * this.altura;
+
+    }
+
+    public double getVolumen() {
+        return super.getArea() * this.altura;
+
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        g.setColor(Color.GREEN);
+
+        g.fillOval(getX(), (int) (getY() + this.altura), (int) super.getRadio(), (int) super.getRadio());
+
+        g.setColor(Color.GREEN);
+        g.drawLine((int) (super.getX() + super.getRadio()), (int) (super.getY() + (super.getRadio() / 2)), (int) (super.getX() + super.getRadio()), (int) (super.getY() + this.altura + super.getRadio() - (super.getRadio() / 2)));
+        g.drawLine((int) (getX()), (int) (super.getY() + (super.getRadio() / 2)), (int) (super.getX()), (int) (super.getY() + this.altura + super.getRadio() - (super.getRadio() / 2)));
+
+    }
+
+    @Override
+    public String toString() {
+        return "\nCilindro" + " \nAltura= " + this.altura + super.toString();
+    }
+
 }
